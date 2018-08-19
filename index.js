32d608c180dae82ebea63720136f7646c854da2d
Carlos Gregorio Ramirez Santos <cgrs27@gmail.com>
2018-08-19 12:06:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+let bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({ extended: true }));
+
 var Animal = require('./Animal.js');
 var Toy = require('./Toy.js');
 
+app.use('/public', express.static('public'));
+
+app.use('/createAnimal',(req,res)=>{
+	if(req.body.femenino='on')
+		req.body.gender='female';
+	else
+		req.body.gender='male';
+
+	let newAnimal = new Animal(
+		req.body
+	)
+	console.log(req.body)
+	console.log(newAnimal);
+	newAnimal.save((err)=>{
+		if(err){
+			res.type('html').status(500);
+			res.send('Error: '+ err);
+		}else{
+			res.send('Nuevo animal creado.');
+		}
+	});
+});
+
 
 app.use('/', (req, res) => {
+	
 	res.json({ msg : 'It works!' });
     });
 
